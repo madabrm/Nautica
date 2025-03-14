df114a16225d50a790f025a6e842026dd165002f
madabrm <83285002+madabrm@users.noreply.github.com>
2025-03-14 23:54:28 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "madabrm.my.id"; // Ganti dengan domain utama kalian
+const rootDomain = "madabrm12.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "nautica"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
